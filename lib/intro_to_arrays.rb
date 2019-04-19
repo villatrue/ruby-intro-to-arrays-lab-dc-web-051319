dbb1cdf70dd717510f92bee08667f0334a44517c
Sean Carter <scarter0313@gmail.com>
2019-04-19 04:28:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,6 +7,8 @@ def array_with_two_elements
 end
 
 def first_element(taylor_swift)
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[0]
 end
+
+
+  
